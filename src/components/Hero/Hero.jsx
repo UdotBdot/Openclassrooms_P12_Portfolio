f6fb94542e076883fb74873fd8209757ed8ea247
UdotBdot <Frederic.sin@proton.me>
2024-03-18 18:13:44 +0100

    UPDATE: Navbar

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Hero.scss'
+
+function Hero() {
+  return (
+    <>
+    {/* <article className='Hero'>
+        <h1 className='Hero__h1'>Enchanté et bienvenue sur mon Portfolio !</h1>
+        <p className='Hero__p'>
+            Je suis Frédéric SIN, un jeune développeur Agile basé en Alsace.<br/>
+            Bienvenue dans mon espace numérique où je m'exprime régulièrement à travers mes projets, mettant en avant ma capacité à combiner esthétique et fonctionnalité pour offrir la meilleure expérience utilisateur possible.<br/>
+            Grâce à mon spèctre de compétences, je propose une diversité de services incluant le débogage, l'optimisation SEO/Performance, ainsi que la création d'applications web.<br/>
+            Explorez mon portfolio pour découvrir ma vision du développement web et ensemble, transformons ensemble vos idées en réalité numérique...
+        </p>
+    </article> */}
+        
+    </>
+  )
+}
+
+export default Hero

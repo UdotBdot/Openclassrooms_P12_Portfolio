6ab16bb8168a994a18870b903b53a7a02fc62aab
UdotBdot <Frederic.sin@proton.me>
2024-03-19 11:44:35 +0100

    FIX: Error Loader

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
-import './Hero.scss'
+import React from 'react';
+import './Hero.scss';
 
 function Hero() {
   return (
-    <>
     <article className='Hero'>
-        <h1 className='Hero__h1'>Welcome to my Portfolio!</h1>
-           <h2 className='Hero__h2'>I am Frederic SIN, a young Agile developer based in Alsace.</h2>
-        <p className='Hero__p'>
-           Welcome to my digital space where I regularly express myself through my projects, highlighting my ability to combine aesthetics and functionality to offer the best possible user experience.
-        </p>
-        <p className='Hero__p'>
-          Thanks to my range of skills, I offer a diversity of services including debugging, SEO/Performance optimization, as well as the creation of web applications.
-        </p>
-        <p className='Hero__p'>
+      <h1 className='Hero__h1'>Welcome to my Portfolio!</h1>
+      <h2 className='Hero__h2'>I am Frederic SIN, a young Agile developer based in Alsace.</h2>
+      <p className='Hero__p'>
+        Welcome to my digital space where I regularly express myself through my projects, highlighting my ability to combine aesthetics and functionality to offer the best possible user experience.
+      </p>
+      <p className='Hero__p'>
+        Thanks to my range of skills, I offer a diversity of services including debugging, SEO/Performance optimization, as well as the creation of web applications.
+      </p>
+      <p className='Hero__p'>
         Explore my portfolio to discover my vision of web development and together, let's transform your ideas into digital reality...
-        </p>
+      </p>
     </article>
-    </>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
--- src/pages/Loader/Loader.jsx
+++ src/pages/Loader/Loader.jsx
@@ -1,122 +1,125 @@
 import React, { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import './Loader.scss';
-import '../../assets/Styles/Fonts.scss'
+import '../../assets/Styles/Fonts.scss';
 
 function Loader() {
   const loaderRef = useRef(null);
   const navigate = useNavigate();
 
-  useEffect( () => {
-    setTimeout(() => {
-      navigate('/home')
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      navigate('/home');
     }, 5300);
-  })
-  
+
+    return () => clearTimeout(timeout);
+  }, [navigate]);
 
   useEffect(() => {
     const Messenger = function (el) {
-        const codeletters = "AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbn123456789";
-        let messageIndex = 0;
-        let currentLength = 0;
-        let fadeBuffer = false;
-        const messages = [
-          'Bienvenue',
-          'Welcome',
-          '歡迎 (ni-hao)',
-        ];
-      
-        const generateRandomString = (length) => {
-          let randomText = '';
-          while (randomText.length < length) {
-            randomText += codeletters.charAt(Math.floor(Math.random() * codeletters.length));
-          }
-          return randomText;
-        };
-      
-        const animateIn = () => {
-          if (currentLength < messages[messageIndex].length) {
-            currentLength += 2;
-            if (currentLength > messages[messageIndex].length) {
-              currentLength = messages[messageIndex].length;
-            }
-            const message = generateRandomString(currentLength);
-            el.current.innerHTML = message;
-            setTimeout(animateIn, 20);
-          } else {
-            setTimeout(animateFadeBuffer, 20);
-          }
-        };
-        const animateFadeBuffer = () => {
-          if (!fadeBuffer) {
-            fadeBuffer = [];
-            for (let i = 0; i < messages[messageIndex].length; i++) {
-              fadeBuffer.push({ c: (Math.floor(Math.random() * 12)) + 1, l: messages[messageIndex].charAt(i) });
-            }
-          }
-      
-          let doCycles = false;
-          let message = '';
-      
-          for (let i = 0; i < fadeBuffer.length; i++) {
-            const fader = fadeBuffer[i];
-            if (fader.c > 0) {
-              doCycles = true;
-              fader.c--;
-              message += codeletters.charAt(Math.floor(Math.random() * codeletters.length));
-            } else {
-              message += fader.l;
-            }
-          }
-      
-          el.current.innerHTML = message;
-      
-          if (doCycles) {
-            setTimeout(animateFadeBuffer, 50);
-          } else {
-            setTimeout(cycleText, 1000);
-          }
-        };
-      
-        const cycleText = () => {
-          messageIndex = (messageIndex + 1) % messages.length;
-          currentLength = 0;
-          fadeBuffer = false;
-          el.current.innerHTML = '';
-          setTimeout(animateIn, 200);
-        };
-      
-        animateIn();
+      const codeletters = "AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbn123456789";
+      let messageIndex = 0;
+      let currentLength = 0;
+      let fadeBuffer = false;
+      const messages = [
+        'Bienvenue',
+        'Welcome',
+        '歡迎 (ni-hao)',
+      ];
+
+      const generateRandomString = (length) => {
+        let randomText = '';
+        while (randomText.length < length) {
+          randomText += codeletters.charAt(Math.floor(Math.random() * codeletters.length));
+        }
+        return randomText;
       };
+
+      const animateIn = () => {
+        if (!document.contains(el.current)) return;
+        if (currentLength < messages[messageIndex].length) {
+          currentLength += 2;
+          if (currentLength > messages[messageIndex].length) {
+            currentLength = messages[messageIndex].length;
+          }
+          const message = generateRandomString(currentLength);
+          el.current.innerHTML = message;
+          setTimeout(animateIn, 20);
+        } else {
+          setTimeout(animateFadeBuffer, 20);
+        }
+      };
+
+      const animateFadeBuffer = () => {
+        if (!document.contains(el.current)) return;
+        if (!fadeBuffer) {
+          fadeBuffer = [];
+          for (let fader of messages[messageIndex]) {
+            fadeBuffer.push({ c: (Math.floor(Math.random() * 12)) + 1, l: fader });
+          }
+        }
+
+        let doCycles = false;
+        let message = '';
+
+        for (let fader of fadeBuffer) {
+          if (fader.c > 0) {
+            doCycles = true;
+            fader.c--;
+            message += codeletters.charAt(Math.floor(Math.random() * codeletters.length));
+          } else {
+            message += fader.l;
+          }
+        }
+
+        el.current.innerHTML = message;
+
+        if (doCycles) {
+          setTimeout(animateFadeBuffer, 50);
+        } else {
+          setTimeout(cycleText, 1000);
+        }
+      };
+
+      const cycleText = () => {
+        if (!document.contains(el.current)) return;
+        messageIndex = (messageIndex + 1) % messages.length;
+        currentLength = 0;
+        fadeBuffer = false;
+        el.current.innerHTML = '';
+        setTimeout(animateIn, 200);
+      };
+
+      animateIn();
+    };
 
     const messenger = new Messenger(loaderRef);
 
-    return () => {
-    };
-  }, []); 
+    return () => {};
+  }, []);
 
   return (
-    <>
     <div className='Loader'>
       <div className="Loader__bulb-container">
         <div className='Loader__wire'></div>
         <div className='Loader__connector'>
-          <div className='Loader__grove'></div>
-          <div className='Loader__grove'></div>
-          <div className='Loader__grove'></div>
+          {[...Array(3)].map((_, index) => (
+            <div key={index} className='Loader__grove'></div>
+          ))}
         </div>
         <div className='Loader__bulb'>
-          <div className='Loader__metal-wire'></div>
-          <div className='Loader__metal-wire'></div>
-          <div className='Loader__metal-wire'></div>
+          {[...Array(3)].map((_, index) => (
+            <div key={index} className='Loader__metal-wire'></div>
+          ))}
         </div>
       </div>
       <div className='Loader__text' ref={loaderRef}></div>
-      <div className="Loader__progress"><FontAwesomeIcon className='Loader__spinner' icon={faSpinner} /></div>
+      <div className="Loader__progress">
+        <FontAwesomeIcon className='Loader__spinner' icon={faSpinner} />
+      </div>
     </div>
-    </>
   );
 }
 

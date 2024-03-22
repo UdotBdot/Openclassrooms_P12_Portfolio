e1f04877598dc287a9f44a9221edb53c23e1d7eb
UdotBdot <Frederic.sin@proton.me>
2024-03-22 12:54:31 +0100

    FIX: Autoslider

--- src/components/AutoSlider/AutoSlider.jsx
+++ src/components/AutoSlider/AutoSlider.jsx
@@ -1,24 +1,42 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './AutoSlider.scss'; 
 
 const AutoSlider = () => {
-  const initialUrl = ['../../public/images/icons/html.png', '../../public/images/icons/css-3.png', '../../public/images/icons/sass.png', '../../public/images/icons/js.png', '../../public/images/icons/react.png', '../../public/images/icons/redux.png', '../../public/images/icons/jest.svg', '../../public/images/icons/figma.png', '../../public/images/icons/notion.png', '../../public/images/icons/mongodb.png', '../../public/images/icons/notion.png', '../../public/images/icons/docker.png'];
+  const initialUrl = ['../../images/html.png', '../../images/css-3.png', '../../images/sass.png', '../../images/js.png', '../../images/react.png', '../../images/redux.png', '../../images/jest.svg', '../../images/figma.png', '../../images/notion.png', '../../images/mongodb.png', '../../images/docker.png'];
   const [Urls, setUrls] = useState(initialUrl);
+  const scrollerRef = useRef();
 
   useEffect(() => {
+    const scrollerElement = scrollerRef.current;
+
     if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-      const duplicatedWords = [...initialUrl, ...initialUrl];
-      setUrls(duplicatedWords);
+      addAnimation(scrollerElement);
     }
+    
+    function addAnimation(scrollerElement) {
+      if (!scrollerElement) return;
+
+      scrollerElement.setAttribute("data-animated", true);
+
+      const scrollerInner = scrollerElement.querySelector(".scroller__inner");
+      const scrollerContent = Array.from(scrollerInner.children);
+
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        duplicatedItem.setAttribute("aria-hidden", true);
+        scrollerInner.appendChild(duplicatedItem);
+      });
+    }
+    
   }, []); 
 
   return (
     <div className='Auto-slider-container'>
-      <div className="Auto-slider">
-      <h2>Front-End Tools</h2>
-        <ul className="Auto-slider__word-list">
+      <div className="scroller" data-speed="slow" ref={scrollerRef}>
+        <h2>Front-End Tools</h2>
+        <ul className="tag-list scroller__inner">
           {Urls.map((url, index) => (
-            <li key={index}><img width={50} height={50} src ={url}/></li>
+            <li key={index}><img width={50} height={50} src={url} alt={`Front-end Tool ${index}`} /></li>
           ))}
         </ul>
       </div>

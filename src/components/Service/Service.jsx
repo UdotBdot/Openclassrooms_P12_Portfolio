4237da20950efc169db86e65d003cc2d980f7525
UdotBdot <Frederic.sin@proton.me>
2024-03-28 15:21:33 +0100

    FIX: CLS

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function Service({text, title, src, alt, width, height}) {
+function Service({text, title, src, alt, width, height, loading}) {
   return (
    <>
     <div className='Services__items'>
-        <img width={width} height={height} alt={alt} src={src}></img>
+        <img loading={loading} width={width} height={height} alt={alt} src={src}></img>
         <h3>{title}</h3>
         <p>{text}</p>
     </div>
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -7,13 +7,13 @@ function Services() {
     <section className='Article'>
          <h2 id="services">My Prestations</h2>
          <div className='Services'>
-            <Service width={64} height={64} alt={"Build App Logo"} src={"../../images/Logos/build.webp"} title={"Web/App"} text="Crafting versatile, Mobile-First and Desktop-First Web/App solutions tailored for diverse objectives." />
+            <Service loading={"lazy"} width={64} height={64} alt={"Build App Logo"} src={"../../images/Logos/build.webp"} title={"Web/App"} text="Crafting versatile, Mobile-First and Desktop-First Web/App solutions tailored for diverse objectives." />
 
-            <Service width={64} height={64} alt={"SEO/Performance Logo"} src={"../../images/Logos/performance.webp"} title={"SEO/Performance"} text="Implementing SEO strategies and min/max Performances ensuring online visibility, and optimal experience."/>
+            <Service loading={"lazy"} width={64} height={64} alt={"SEO/Performance Logo"} src={"../../images/Logos/performance.webp"} title={"SEO/Performance"} text="Implementing SEO strategies and min/max Performances ensuring online visibility, and optimal experience."/>
 
-            <Service width={64} height={64} alt={"Debug Logo"} src={"../../images/Logos/bugs.webp"} title={"Debug"} text="Facing challenges with your Web App? My comprehensive service includes an assessment of encountered bugs and their resolution within the scope of my expertise."/>
+            <Service loading={"lazy"} width={64} height={64} alt={"Debug Logo"} src={"../../images/Logos/bugs.webp"} title={"Debug"} text="Facing challenges with your Web App? My comprehensive service includes an assessment of encountered bugs and their resolution within the scope of my expertise."/>
 
-            <Service width={64} height={64} alt={"Agile dev Logo"}e src={"../../images/Logos/agile.webp"} title={"Agile developer"} text="As an Agile developer, I am dedicated to delivering flexible, collaborative, and quality-focused solutions, establishing swift adaptation to evolving project requirements."/>
+            <Service loading={"lazy"} width={64} height={64} alt={"Agile dev Logo"}e src={"../../images/Logos/agile.webp"} title={"Agile developer"} text="As an Agile developer, I am dedicated to delivering flexible, collaborative, and quality-focused solutions, establishing swift adaptation to evolving project requirements."/>
          </div>
     </section>
   )

3e08d4cd1c8976efbf2b553a553ffab53dfce551
UdotBdot <Frederic.sin@proton.me>
2024-04-23 13:06:16 +0200

    FIX: cookie pt2

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ document.cookie = "__Secure-3PAPISID=vU7nyPTenO1BzUcT/AoE0MDdzSkwVYJP77; SameSit
 document.cookie = "__Secure-3PSID=g.a000iwjsCEArWM1Ave0JQ3lXY87wvRue33_ZXM2A9cvrhpfajEOmvdhoNPEv7juNpZD7wwNYeAACgYKAeQSAQASFQHGX2MiUsuYe70XHw0AJtLJCKrjzxoVAUF8yKq3qXEtIgWlc-GdMGxQ3q8x0076; SameSite=None; Secure; Partitioned;";
 document.cookie = "__Secure-3PSIDCC=AKEyXzXDt1OZ8BnZDKvprhPduEC58motjApiXdKUYugXOCGz9zbT6_hftz4he4_qOqpa1WC3YPo; SameSite=None; Secure; Partitioned;";
 document.cookie = "__Secure-3PSIDTS=sidts-CjIB7F1E_Al-gVDfrP97MQEED4SxNw1-Xg4b7gccBjXiP9526gOg7dYyMymFRBBuiEtIiRAA; SameSite=None; Secure; Partitioned;";
+document.cookie="_GRECAPTCHA=09AM6hZhZi5LMuyX-5hvkjl7TvB_yclyN-3d898-yFz_dcar3eZxRlJtsWo0e_kL2HI7cDx7QjHYrEpUCW39d2lRM; Secure; Path=/; SameSite=None; Partitioned;"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
